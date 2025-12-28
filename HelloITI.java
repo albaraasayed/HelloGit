2418c7d762d52f05d7852cb8310649d4d07d687d
Albaraa Alsayed <albaraaalsayed4@gmail.com>
2025-12-28 12:45:57 +0200

    PrintHelloGit function added to HelloITI class

--- HelloITI.java
+++ HelloITI.java
@@ -1,4 +1,5 @@
 class Main {
+    void printHelloGit() {System.out.println("Hello git");}
     
     void printHello(){System.out.println("Hello ITI");}
     public static void main(String []args) {
