026e821ad4cb2a0c5e53e64776a23a0000fcaaf1
abdelrahman <abdelrahmankarim7@gmail.com>
2025-12-30 12:05:49 +0200

    add print hi akm method

--- HelloITI.java
+++ HelloITI.java
@@ -1,5 +1,6 @@
 class Main {
     void printHelloGit() {System.out.println("Hello git");}
+    void printHiAkm(){System.out.println("Hello Akm");}
     void printHiGitHub() {System.out.println("Hi GitHub");}
     
     void printHello(){System.out.println("Hello ITI");}
