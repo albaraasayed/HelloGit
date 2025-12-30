439608b0c020d6a05bac0fe3027fa3b514dca40e
mazen675 <139044003+mazen675@users.noreply.github.com>
2025-12-30 11:34:56 +0200

    method added printHelloMazen() in line 3 to HelloITI.java

--- HelloITI.java
+++ HelloITI.java
@@ -1,6 +1,6 @@
 class Main {
     void printHelloGit() {System.out.println("Hello git");}
-    
+    void printHelloMazen(){System.out.println("Hello Mazen");}
     void printHello(){System.out.println("Hello ITI");}
     public static void main(String []args) {
         
