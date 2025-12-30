521823c73e402dd4616a78c2125ac8f0a23cd77c
Albaraa Alsayed <albaraaalsayed4@gmail.com>
2025-12-30 11:30:09 +0200

    in line 3 we added print hi function

--- HelloITI.java
+++ HelloITI.java
@@ -1,5 +1,6 @@
 class Main {
     void printHelloGit() {System.out.println("Hello git");}
+    void printHiGitHub() {System.out.println("Hi GitHub");}
     
     void printHello(){System.out.println("Hello ITI");}
     public static void main(String []args) {
